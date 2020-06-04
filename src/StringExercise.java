fb15fe48ef424e67ea41ec4afc5b64e730ba8536
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-06-04 15:51:25 -0500

    adding string lecture note changes and adding exercise 1

---
+++ src/StringExercise.java
@@ -0,0 +1,17 @@
+
+public class StringExercise {
+    public static void main(String[] args){
+        //exercise one
+        String one = "We don't need no education \nWe don't need no thought control";
+        System.out.println(one);
+
+        String two = "Check \"this\" out!, \"s inside of \"s";
+        System.out.println(two);
+
+        String three = "In windows, the main drive is usually C:\\";
+        System.out.println(three);
+
+        String four = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(four);
+    }
+}
