2fab6f1ded01c5e48b92c5b48e720b3cd90c78a8
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-06-07 18:57:59 -0500

    adding method exercise practice

---
+++ src/MethodsExercises.java
@@ -0,0 +1,8 @@
+public class MethodsExercises {
+    public void main(String[] args){
+        System.out.println("My name is Augustine.");
+    }
+    public static void printFirstName(){
+
+    }
+}
