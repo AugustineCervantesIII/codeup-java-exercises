3aeb6b59d13693d386b10471eabee2086d173371
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-06-09 09:25:09 -0500

    adding practice file

---
+++ src/JavaPractice.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class JavaPractice {
+    public static void main(String[] args){
+        System.out.println("Hello\nAugustine Cervantes");
+        System.out.println(74 + 36);
+        System.out.println(50 / 3);
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + -3 * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        System.out.println("enter first number: ");
+        System.out.println("enter second number: ");
+        System.out.println("");
+    }
+}
